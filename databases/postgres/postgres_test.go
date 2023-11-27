98d8f3edbe7e658fb0512e1aa12913911c36ccf0
Greg JEFTIC <ajkula@gmail.com>
2023-11-27 02:14:14 +0100

    adding first tests

--- databases/postgres/postgres_test.go
+++ databases/postgres/postgres_test.go
@@ -8,8 +8,6 @@ import (
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 
-	"github.com/DATA-DOG/go-sqlmock"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -90,23 +88,21 @@ func TestPostgresConnector_Connect(t *testing.T) {
 }
 
 func TestPostgresConnector_GetTableNames(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("Erreur lors de la création de l'instance db, mock: %s", err)
-	}
-	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
-	if err != nil {
-		t.Fatalf("Erreur lors de la création de l'instance GORM: %s", err)
-	}
+	// Démarrage du conteneur PostgreSQL et récupération des informations de connexion
+	postgresContainer, host, port, database, user, password := startPostgresContainer(t)
+	defer (*postgresContainer).Terminate(context.Background())
 
-	// Configurer le mock pour simuler une réponse pour GetTableNames
-	mock.ExpectQuery("^SELECT table_name").WillReturnRows(sqlmock.NewRows([]string{"table_name"}).AddRow("table1").AddRow("table2"))
-
-	// Test de GetTableNames
+	// Connexion au conteneur PostgreSQL
 	connector := PostgresConnector{}
-	tableNames, err := connector.GetTableNames(gormDB)
+	db, err := connector.Connect(host, port, database, user, password)
 	assert.NoError(t, err)
-	assert.Equal(t, []string{"table1", "table2"}, tableNames)
+
+	err = createTestSchema(db)
+	assert.NoError(t, err)
+
+	tableNames, err := connector.GetTableNames(db)
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"table1", "table2", "table3"}, tableNames)
 }
 
 func TestPostgresConnector_Connect_error(t *testing.T) {
@@ -114,8 +110,8 @@ func TestPostgresConnector_Connect_error(t *testing.T) {
 
 	// Test avec des paramètres invalides
 	_, err := connector.Connect("invalidHost", "invalidPort", "invalidDatabase", "invalidUser", "invalidPassword")
-	assert.Error(t, err, "Une connexion avec des paramètres invalides devrait échouer")
+	assert.Error(t, err, "Invalid parameters should fail to connect")
 
 }
 
-// Autres tests pour GetTableMetadata, GetIndexes, etc.
+// GetTableMetadata, GetIndexes, etc.
